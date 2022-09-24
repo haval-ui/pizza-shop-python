14ac74d5ca87f71d07a15ba9cf965cd189d48a96
haval <havalkoet00908@gmail.com>
2022-09-24 01:21:11 -0700

    the initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? s, m, or l ")
+add_pepperoni = input("Do you want pepperoni? y or n ")
+extra_cheese = input("Do you want extra cheese? y or n ")
+
+
+
+if size=="s":
+    bill=15
+    if add_pepperoni=="y":
+        bill+=2
+    
+
+elif size=="m":
+    bill=20
+    if add_pepperoni=="y":
+        bill+=3
+
+else:
+    bill=25
+    if add_pepperoni=="y":
+        bill+=3
+
+if extra_cheese=="y":
+    bill+=1
+    print(f"yor bill is ${bill}")
+else:
+    print(f"yor bill is ${bill}")
+
+
